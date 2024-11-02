ec1cd11ab6f0cca7fef7988127400f4253676a0e
PhuongLy <phuong.ly.k2set@gmail.com>
2024-11-02 17:20:42 +0700

    Practice-03: setup _db.js as a mock database, implement basic handlers to to return all reviews, all author or all games

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
 
-import { typeDefs } from "./schema";
+import { typeDefs } from "./schema.js";
+import db from "./_db.js" //mock database
+
+const resolvers = {
+    Query:{
+        games(){ //must be the same name as the entry point defined in the Schema Query type (schem.js file)
+            return db.games //return value must be the same type with what defined in the Schema Query type
+        },
+        authors(){
+            return db.authors
+        },
+        reviews(){
+            return db.reviews
+        }
+    }
+}
 
 //server setup
 const server = new ApolloServer({
-    typeDefs    //defines how the Graph (obj nodes) looks like including their relationship
-    //resolvers //defines handler to handle the requests
+    typeDefs,    //defines how the Graph (obj nodes) looks like including their relationship
+    resolvers //defines handlers to handle the requests
 })
 
 const {url} = await startStandaloneServer(server,{
